823a539e9f9e42c31186b4c99dc6114609a81d10
patelheta <patelheta385@gmail.com>
2022-10-04 15:11:35 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -18,16 +18,32 @@
 // } else {
 //   console.log("Short sleeves are fine.");
 // }
-const temperature = 18;
+// const temperature = 18;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else if (temperature < 20) {
-  console.log("make sure you bring your huddy");
-} else {
-  console.log("Short sleeves are fine.");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else if (temperature < 20) {
+//   console.log("make sure you bring your huddy");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// const isCitizen = false;
+// const age = 26;
+// const country = "India";
+
+// if (isCitizen && age > 18 || country=="India") {
+//   console.log("You are eligible to vote.");
+// }
+// const temperature = 50;
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea…");
+// }
+const raining = false
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
-
-console.log("Now you're ready to go outside!");

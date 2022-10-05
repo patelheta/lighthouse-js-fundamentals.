9937b7387e74f57aacdc5b597f3cccfe11b6a611
patelheta <patelheta385@gmail.com>
2022-10-04 22:19:14 -0700

    Creat say hello function Js

---
+++ loopy.js
@@ -0,0 +1,22 @@
+// for (let num = 100; num <= 200; num++) {
+//   if (num % 3 == 0) {
+//     console.log("loopy");
+//   } else if (num % 4 == 0) {
+//     console.log("Lighthouse");
+//   } else if (num % 3 == 0 && num % 4 == 0) {
+//     console.log("LoopyLighthouse");
+//   } else {
+//     console.log(num);
+//   }
+// }
+for (let num = 100; num <= 200; num++) {
+  if (num % 3 == 0 && num % 4 == 0) {
+    console.log("LoopyLighthouse");
+  } else if (num % 4 == 0) {
+    console.log("Lighthouse");
+  } else if (num % 3 == 0) {
+    console.log("Loopy");
+  } else {
+    console.log(num);
+  }
+}

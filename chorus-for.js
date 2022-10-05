be716df2edf18b09d1f3e324fc9d05e3b37002de
patelheta <patelheta385@gmail.com>
2022-10-04 22:20:16 -0700

    Implement for...of

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,13 @@
-const chorus = "Let's dance!";
-for (let repeat = 0; repeat < 10; repeat++) {
-  console.log(chorus);
+// const chorus = "Let's dance!";
+// for (let repeat = 0; repeat < 10; repeat++) {
+//   console.log(chorus);
+// }
+// console.log("Until the sun comes up!");
+
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+let total = 0;
+for (let amount of amounts) {
+  console.log(amount);
+  total += amount; // total = total + amount
 }
-console.log("Until the sun comes up!");
+console.log('Order total is: ', total);
